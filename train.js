40cda0352d6ff121b81d00b80919d6c7fae12d53
ozodovazamat <ozodovazamat@Ozodovs-MacBook-Pro.local>
2026-04-20 17:03:22 +0900

    feat: MIT B task

--- train.js
+++ train.js
@@ -1,3 +1,28 @@
+/* B-TASK (NodeJS)
+
+Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
+MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi. */
+
+
+// ⭐️ Masalaning yechimi
+
+
+function countNums(str) {
+  return str.split("").map(Number).filter(Boolean).length;
+}
+
+
+
+console.log(countNums("gwefkwgiefgwkb886kdhjk"));
+
+
+
+
+
+
+
+
+
 /* A- Task (NodeJS)
 
 Savol: Shunday 2 parametrli functionlarni tuzing,
@@ -6,11 +31,11 @@ Masalan countLetter("e", "engineer") 3ni return qiladi. */
 
 //parameter letter words
 
-function countLetters (letter, word) {
-    const splittedWord = word.split(" ")
-    const filteredWord = splittedWord = splittedWord.filter((ele) => {return ele === letter})
-    return filteredWord.length
+//function countLetters (letter, word) {
+  //  const splittedWord = word.split("")
+    //const filteredWord = splittedWord.filter((ele) => {return ele === letter})
+   // return filteredWord.length
 
-}
+//}
 
-console.log(countLetters("letter", "word"))
+//console.log(countLetters("letter", "word"))
