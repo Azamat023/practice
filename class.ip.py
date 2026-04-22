da94afdd408ece9092712db96167082863c20d79
ozodovazamat <ozodovazamat@Ozodovs-MacBook-Pro.local>
2026-04-22 16:08:06 +0900

    feat: Polimorphism concept

--- class.ip.py
+++ class.ip.py
@@ -34,6 +34,9 @@ class Dog(Animal):#child
     def protect(self):
         print("Yes, I can protect you")
 
+    def make_voice(self):
+        print(f"the {self.name} says {self.sound}")
+
       
 
 class Cat(Animal): #child 
@@ -89,3 +92,23 @@ print(Dog.description)
 print(dog.voice, fish.voice)
 print("dog.status:", dog._status)
 print("cat.status:", cat._status)
+
+
+print("==== POLIMORPHISM =====")
+
+dog.make_voice()
+fish.make_voice()
+
+print("----")
+# fish > Fish > Animal > Object
+a = isinstance(fish, Fish)
+b = isinstance(fish, Animal)
+c = isinstance(fish, object)
+d = isinstance("MIT", object)
+result = a and b and c and d
+print(f"the result: {result}")
+
+#Fish > Animal >Object
+data1 = issubclass(Fish, Animal)
+data2 = issubclass(Animal, object)
+print("data:", data1, data2)
