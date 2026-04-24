71ff7f5387913c54352d15ddc5091e426ad2f197
ozodovazamat <ozodovazamat@Ozodovs-MacBook-Pro.local>
2026-04-24 17:40:57 +0900

    fix:MITASK- D

--- train.js
+++ train.js
@@ -1,6 +1,19 @@
 function getHighestIndex(arr) {
-  return arr.indexOf(Math.max(...arr));
+  let max = arr[0];
+  let index = 0;
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+      index = i;
+    }
+  }
+
+  return index;
 }
+
+// Test
+console.log(getHighestIndex([5, 21, 12, 21, 8])); // 1
 
 
 
