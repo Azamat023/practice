6c2c5b7cfe9a11cebcd001c586547623e12fcb94
ozodovazamat <ozodovazamat@Ozodovs-MacBook-Pro.local>
2026-05-01 15:28:53 +0900

    feat: building simple backend server on nodejs

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
-// NodeJS EXPRESS NESTJS
-// PYTHON 
+const express = require('express')
+const app = express()
+
+app.get('/',(req, res) => {
+    res.send('<h1>Hello World!<h1>')
+})
+
+app.get('/rest', (req, res) => {
+  res.json({name: "Marco", age: 21, group: "MIT"})
+})
+
+app.listen(3000, () => {
+  console.log(`Backend Server is running on port 3000`)
+})
