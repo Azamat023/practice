ff5fc9456c69c613bd3b6c2bf4d88335661aa174
ozodovazamat <ozodovazamat@Ozodovs-MacBook-Pro.local>
2026-05-01 16:43:18 +0900

    fix: Promise and Callback hell

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 
+//Traditional API
 app.get('/',(req, res) => {
     res.send('<h1>Hello World!<h1>')
 })
 
+// Rest API
 app.get('/rest', (req, res) => {
   res.json({name: "Marco", age: 21, group: "MIT"})
 })
